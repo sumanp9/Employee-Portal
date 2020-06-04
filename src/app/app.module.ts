1b221df11d865cf9c4ceb25147a7ca11500a37e3
sumanp9 <11.sumanpradhan@gmail.com>
2020-06-04 11:34:55 -0500

    Added Angular material and some html inputs amd buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MatButtonModule} from '@angular/material/button'; 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
