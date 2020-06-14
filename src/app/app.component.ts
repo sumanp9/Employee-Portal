9605d4424bd40c91fd8f639f391d4d61f8006eb3
sumanp9 <11.sumanpradhan@gmail.com>
2020-06-14 16:39:45 -0500

    Added angular bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {element} from "protractor";
+
+declare const FB: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  token: any;
+  logged: boolean = false;
+  user = {name: 'Hello'};
   loggedIn: boolean;
   userName: string;
   password: string;
@@ -14,13 +20,25 @@ export class AppComponent {
   constructor(private route: Router) {
   }
 
+  statusChangeCallback(response: any) {
+    if (response.status = 'connected'){
+      console.log("Connected!!");
+    } else {
+      this.login();
+    }
+  }
+ // Use this URL: https://dzone.com/articles/login-with-facebook-and-google-using-angular-8
 
   login() {
-    if (this.userName == "user" && this.password == "pass"){
+    FB.login(function (result) {
+      this.logged = true;
+      this.token = result;
+    }, {scope: 'user'});
+    /*if (this.userName == "user" && this.password == "pass"){
       this.loggedIn = true;
     } else {
       alert("Incorrect username or password");
-    }
+    }*/
   }
 
   openBottomSheet() {
